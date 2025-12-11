d496f7e9eb595b70b0983fdd663e46140104b5c3
0xNullll <175228537+0xNullll@users.noreply.github.com>
2025-12-11 23:16:22 +0200

    Add GPL license headers to all source and header files
    Added GNU General Public License v3 headers and project metadata to all C source and header files in the CryptoForge project for improved licensing clarity and attribution. No functional code changes were made.

--- crypto/kdf/hkdf.h
+++ crypto/kdf/hkdf.h
@@ -1,3 +1,23 @@
+/*
+ * CryptoForge - hkdf.h / HKDF (HKDF-SHA-1, HKDF-SHA-2, and HKDF-SHA-3) Interface
+ * Copyright (C) 2025 0xNullll
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Project repository: https://github.com/0xNullll/CryptoForge
+ */
+
 #ifndef HKDF_H
 #define HKDF_H
 
