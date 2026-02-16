0825dd660fe343423a8ef2081e3cda236d2091e0
0xNullll <agent.snow.1982@gmail.com>
2026-02-16 16:03:13 +0200

    Update cf_aead.c

--- crypto/cf_api/cf_aead.c
+++ crypto/cf_api/cf_aead.c
@@ -46,7 +46,7 @@ static bool aes_gcm_final_wrapper(const CF_AEAD_CTX *ctx, uint8_t *tag, size_t t
 
 // ChaCha20-Poly1305
 static bool chacha20_poly1305_init_wrapper(CF_AEAD_CTX *ctx) {
-    return ll_CHACHA20_POLY13051305_Init((const ll_CHACHA20_POLY1305_CTX *)ctx->aead_ctx, ctx->key, ctx->key_len, ctx->iv, ctx->aad, ctx->aad_len, ctx->operation);
+    return ll_CHACHA20_POLY1305_Init((const ll_CHACHA20_POLY1305_CTX *)ctx->aead_ctx, ctx->key, ctx->key_len, ctx->iv, ctx->aad, ctx->aad_len, ctx->operation);
 }
 static bool chacha20_poly1305_update_wrapper(const CF_AEAD_CTX *ctx, const uint8_t *in, size_t in_len, uint8_t *out) {
     return ll_CHACHA20_POLY1305_Update((const ll_CHACHA20_POLY1305_CTX *)ctx->aead_ctx, in, in_len, out);
@@ -57,7 +57,7 @@ static bool chacha20_poly1305_final_wrapper(const CF_AEAD_CTX *ctx, uint8_t *tag
 
 // XChaCha20-Poly1305
 static bool xchacha20_poly1305_init_wrapper(CF_AEAD_CTX *ctx) {
-    return ll_XCHACHA20_POLY1305_Init((const ll_CHACHA20_POLY1305_CTX *)ctx->aead_ctx, ctx->key, ctx->key_len, ctx->iv, ctx->aad, ctx->aad_len, ctx->operation);
+    return ll_XCHACHA20_POLY1305_Init((const ll_CHACHA20_POLY1305_CTX *)ctx->aead_ctx, ctx->key, ctx->iv, ctx->aad, ctx->aad_len, ctx->operation);
 }
 static bool xchacha20_poly1305_update_wrapper(const CF_AEAD_CTX *ctx, const uint8_t *in, size_t in_len, uint8_t *out) {
     return ll_XCHACHA20_POLY1305_Update((const ll_CHACHA20_POLY1305_CTX *)ctx->aead_ctx, in, in_len, out);
@@ -69,35 +69,36 @@ static bool xchacha20_poly1305_final_wrapper(const CF_AEAD_CTX *ctx, uint8_t *ta
 //
 // XChaCha20-Poly1305
 //
-static const CF_AEAD *CF_get_xchacha20_poly1305(void) {
-    static CF_AEAD aead = {
-        .id = CF_XCHACHA20,
-        .ctx_size = sizeof(ll_XCHACHA20_CTX),
-        .key_ctx_size = 0,
-        .block_size = 0,
-        .cipher_init_fn = xchacha20_init_wrapper,
-        .cipher_enc_fn = xchacha20_cipher_wrapper,
-        .cipher_dec_fn = xchacha20_cipher_wrapper
-    };
-    return &cipher;
-}
+// static const CF_AEAD *CF_get_xchacha20_poly1305(void) {
+//     static CF_AEAD aead = {
+//         .id = CF_XCHACHA20,
+//         .ctx_size = sizeof(ll_XCHACHA20_CTX),
+//         .key_ctx_size = 0,
+//         .block_size = 0,
+//         .cipher_init_fn = xchacha20_init_wrapper,
+//         .cipher_enc_fn = xchacha20_cipher_wrapper,
+//         .cipher_dec_fn = xchacha20_cipher_wrapper
+//     };
+//     return &cipher;
+// }
 
 // Table of all supported AEAD Ciphers
 static const CF_ALGO_ENTRY cf_AEAD_table[] = {
-    { CF_AES_ECB,     (const void* (*)(void))CF_get_aes_ecb    },
-    { CF_AES_CBC,     (const void* (*)(void))CF_get_aes_cbc    },
-    { CF_AES_OFB,     (const void* (*)(void))CF_get_aes_ofb    },
-    { CF_AES_CFB8,    (const void* (*)(void))CF_get_aes_cfb8   },
-    { CF_AES_CFB128,  (const void* (*)(void))CF_get_aes_cfb128 },
-    { CF_AES_CTR,     (const void* (*)(void))CF_get_aes_ctr    },
+    {0, NULL}
+    // { CF_AES_ECB,     (const void* (*)(void))CF_get_aes_ecb    },
+    // { CF_AES_CBC,     (const void* (*)(void))CF_get_aes_cbc    },
+    // { CF_AES_OFB,     (const void* (*)(void))CF_get_aes_ofb    },
+    // { CF_AES_CFB8,    (const void* (*)(void))CF_get_aes_cfb8   },
+    // { CF_AES_CFB128,  (const void* (*)(void))CF_get_aes_cfb128 },
+    // { CF_AES_CTR,     (const void* (*)(void))CF_get_aes_ctr    },
 
-    { CF_CHACHA8,     (const void* (*)(void))CF_get_chacha8    },
-    { CF_CHACHA12,    (const void* (*)(void))CF_get_chacha12   },
-    { CF_CHACHA20,    (const void* (*)(void))CF_get_chacha20   },
+    // { CF_CHACHA8,     (const void* (*)(void))CF_get_chacha8    },
+    // { CF_CHACHA12,    (const void* (*)(void))CF_get_chacha12   },
+    // { CF_CHACHA20,    (const void* (*)(void))CF_get_chacha20   },
 
-    { CF_XCHACHA8,    (const void* (*)(void))CF_get_xchacha8    },
-    { CF_XCHACHA12,   (const void* (*)(void))CF_get_xchacha12   }
-    { CF_XCHACHA20,   (const void* (*)(void))CF_get_xchacha20   }
+    // { CF_XCHACHA8,    (const void* (*)(void))CF_get_xchacha8    },
+    // { CF_XCHACHA12,   (const void* (*)(void))CF_get_xchacha12   }
+    // { CF_XCHACHA20,   (const void* (*)(void))CF_get_xchacha20   }
 };
 
 const CF_AEAD *CF_AEAD_GetByFlag(uint32_t algo_flag) {
